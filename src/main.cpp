ab65e8508adc7ac006e594f0c2f9961f2db577fc
ryuse- <08cpper06@gmail.com>
2024-08-10 16:51:59 +0900

    - read source code from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string> 
 
 
 int main(int argc, const char** argv) {
-	std::cout << "Hello World!!" << std::endl;
+	if (argc == 1) {
+		std::cout << "no input" << std::endl;
+		return 1;
+	}
+	std::ifstream in(argv[1]);
+	if (in.fail()) {
+		std::cout << "could not found file: " << argv[1] << std::endl;
+		return 2;
+	}
+	std::string source((std::istreambuf_iterator<char>(in)), (std::istreambuf_iterator<char>()));
+	source.push_back('\0');
+
+	std::cout << source << std::endl;
+
 	return 0;
 }
